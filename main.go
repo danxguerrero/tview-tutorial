a9e72d7ae03022ba63cfdb2b9760448f22e06573
danxguerrero <danxguerrero@yahpp.com>
2023-12-26 23:22:04 -0500

    Displays current time in terminal

--- main.go
+++ main.go
@@ -1,12 +1,50 @@
 package main
 
 import (
+	"fmt"
+	"time"
+
+
 	"github.com/rivo/tview"
 )
 
-func main() {
-	box := tview.NewBox().SetBorder(true).SetTitle("Hello, world!")
-	if err := tview.NewApplication().SetRoot(box, true).Run(); err != nil {
-		panic(err)
+
+// Set refresh interval to 5 seconds
+const refreshInterval = 500 * time.Millisecond
+
+var (
+	view *tview.Modal
+	app *tview.Application
+)
+
+
+func currentTimeString() string {
+	t := time.Now()
+	return fmt.Sprintf(t.Format("Current time is 15:04:05"))
+}
+
+func updateTIme() {
+	for {
+		time.Sleep(refreshInterval)
+		app.QueueUpdateDraw(func() {
+		view.SetText(currentTimeString())
+		})
 	}
+}
+
+func main() {
+	app = tview.NewApplication()
+	view = tview.NewModal().
+		SetText(currentTimeString()).
+		AddButtons([]string{"Quit", "Cancel"}).
+		SetDoneFunc(func(buttonIndex int, buttonLabel string) {
+			if buttonLabel == "Quit" {
+				app.Stop()
+			}
+		})
+
+		go updateTIme()
+		if err := app.SetRoot(view, false).Run(); err != nil {
+			panic(err)
+		}
 }
